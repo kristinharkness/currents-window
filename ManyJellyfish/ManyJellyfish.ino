186a3f8cf580ea14b1d139b956cf6ee85b4fcd01
Kristin Harkness <kristin@bitsandpixels.com>
2017-06-08 09:04:01 -0400

    checkpoint

--- ManyJellyfish/ManyJellyfish.ino
+++ ManyJellyfish/ManyJellyfish.ino
@@ -15,25 +15,30 @@ typedef struct {
   bool isBrightening;
   int drip1;
   int drip2;
+  byte dripSpeed;
   Adafruit_NeoPixel strip;
 } Jellyfish;
 
-Jellyfish jellies[] = {
-  {9, 253, 3, 30, 10, 80, 10, true, 16, 17}
+static Jellyfish jellies[] = {
+  {9, 253, 3, 10, 80, 10, true, 16, 17, 2}
 };
 
 void setup() {
   Serial.begin(9600);
-  jellies[0].strip = Adafruit_NeoPixel(numLeds, jellies[0].pin, NEO_GRB + NEO_KHZ800);
-  jellies[0].strip.setBrightness(jellies[0].brightness);
-  jellies[0].strip.begin();
-  jellies[0].strip.show(); // Initialize all pixels to 'off'
+
+  for (int idx = 0; idx < 1; idx++) {
+    Jellyfish jelly = jellies[idx];
+    jelly.strip = Adafruit_NeoPixel(numLeds, jellies[0].pin, NEO_GRB + NEO_KHZ800);
+    jelly.strip.setBrightness(jelly.brightness);
+    jelly.strip.begin();
+    jelly.strip.show(); // Initialize all pixels to 'off'
+  }
 }
 
 void loop() {
   //rainbowSlice(100, 90, 180); // blue
   //rainbowSlice(100, 170, 255); // pink
-  rainbowSlice(100); // orange
+  rainbowSlice(100);
 }
 
 void rainbowSlice(uint8_t wait) {
@@ -42,26 +47,25 @@ void rainbowSlice(uint8_t wait) {
 
   for (j = 0; j < numLeds; j++) {
     for (i = 0; i < numLeds; i++) {
-      
+
       pixel = i + j;
       if (pixel >= numLeds) {
         pixel = pixel - numLeds;
       }
 
       for (int idx = 0; idx < 1; idx++) {
-        
+
         Jellyfish jelly = jellies[idx];
-        
-        if (pixel == jelly.drip1 || pixel == jelly.drip2 || pixel == jelly.drip1 - 1 || pixel == jelly.drip2 - 1) {
+
+        if (pixel == jelly.drip1 || pixel == jelly.drip1 - 1 || pixel == jelly.drip2 || pixel == jelly.drip2 + 1) {
           jelly.strip.setPixelColor(pixel, jelly.strip.Color(0, 0, 0, 255 ));
         } else {
           jelly.strip.setPixelColor(pixel, Wheel((i + jelly.first) & 255));
         }
 
-
         jelly.strip.show();
 
-        if (j % 2 == 0) {
+        if (j % jelly.dripSpeed == 0) {
 
           jelly.drip1--;
           if (jelly.drip1 < 0) {
@@ -107,16 +111,4 @@ uint32_t Wheel(byte WheelPos) {
   WheelPos -= 170;
   return jellies[0].strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0, 0);
 }
-
-uint8_t red(uint32_t c) {
-  return (c >> 16);
-}
-uint8_t green(uint32_t c) {
-  return (c >> 8);
-}
-uint8_t blue(uint32_t c) {
-  return (c);
-}
-
-
 
